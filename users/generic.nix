e710d5e2974334ec8886946699719463aa4caf98
Nyctef <nyctef@nyctef.com>
2025-02-26 16:44:05 +0000

    disable ? glob character in fish

--- users/generic.nix
+++ users/generic.nix
@@ -108,6 +108,9 @@ in
 
     # install fish, and tell HM to manage it (set session variables etc)
     programs.fish.enable = true;
+    programs.fish.shellInit = "
+      set -U fish_features qmark-noglob
+    ";
 
     # git stuff
     programs.git = {
