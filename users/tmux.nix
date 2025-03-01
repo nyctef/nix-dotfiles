83b500e426badc3c92755cf03b92206cf6c074f0
Nyctef <nyctef@nyctef.com>
2025-03-01 21:35:16 +0000

    more tmux options
    - set TERM to tmux- instead of screen- (apparently this makes italics
      work)
    - open splits with the same folder
    - start numbering stuff at 1

--- users/tmux.nix
+++ users/tmux.nix
@@ -9,6 +9,19 @@
     # https://superuser.com/a/1809494
     escapeTime = 50;
     historyLimit = 50000;
-    terminal = "screen-256color";
+    terminal = "tmux-256color";
+    extraConfig = ''
+    # make splits open in the current folder
+    bind '"' split-window -v -c "#{pane_current_path}"
+    bind % split-window -h -c "#{pane_current_path}"
+
+    # Start windows and panes at 1, not 0
+    set -g base-index 1
+    set -g pane-base-index 1
+    set-window-option -g pane-base-index 1
+    set-option -g renumber-windows on
+
+
+    '';
   };
 }
