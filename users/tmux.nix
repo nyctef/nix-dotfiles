3f7b0bd70a078026a5671eafb597aa0b188c4854
Nyctef <nyctef@nyctef.com>
2025-03-01 21:02:27 +0000

    copy a few settings from tmux-sensible

--- users/tmux.nix
+++ users/tmux.nix
@@ -1,9 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  programs.tmux.enable = true;
-
-  programs.tmux.keyMode = "vi";
-  # bind prefix + hjkl/HJKL
-  programs.tmux.customPaneNavigationAndResize = true;
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    # bind prefix + hjkl/HJKL
+    customPaneNavigationAndResize = true;
+    # https://superuser.com/a/1809494
+    escapeTime = 50;
+    historyLimit = 50000;
+    terminal = "screen-256color";
+  };
 }
