3b4919ab5a816029929de77c6208c9b4dc2644f9
Nyctef <nyctef@nyctef.com>
2025-03-02 10:19:40 +0000

    reorganise telescope bindings and use it for a couple of lsp actions

--- users/neovim/after/plugin/telescope.lua
+++ users/neovim/after/plugin/telescope.lua
@@ -1,7 +1,23 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-vim.keymap.set('n', '<leader>bu', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ol', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>ne', function()
+    builtin.diagnostics({
+	severity='error',
+	layout_strategy='vertical'
+    })
+end, {})
+
+-- lsp pickers
+-- NOTE: use C-q to dump the results into the quickfix list for reference
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>ic', builtin.lsp_incoming_calls, {})
+vim.keymap.set('n', '<leader>oc', builtin.lsp_outgoing_calls, {})
+
+-- less commonly used, maybe clean up
+vim.keymap.set('n', '<leader>bu', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ff', function()
     builtin.live_grep({
 	prompt_title = 'Find literal string...',
@@ -13,10 +29,3 @@ vim.keymap.set('n', '<leader>fr', function()
 	prompt_title = 'Find regex (via ripgrep) ...',
     })
 end, {})
-vim.keymap.set('n', '<leader>ne', function()
-    builtin.diagnostics({
-	severity='error',
-	layout_strategy='vertical'
-    })
-end, {})
-
