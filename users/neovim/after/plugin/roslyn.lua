90d6e39690ab6f67b0c64cba71f02f1ba04b002c
Nyctef <nyctef@nyctef.com>
2025-03-02 00:10:34 +0000

    this seems to fix roslyn LSP for now

--- users/neovim/after/plugin/roslyn.lua
+++ users/neovim/after/plugin/roslyn.lua
@@ -2,4 +2,13 @@ require('roslyn').setup({
 
 	exe = 'Microsoft.CodeAnalysis.LanguageServer',
 
+	-- TODO: figure out how to make file watching work properly
+	-- currently if we leave filewatching=true here, then roslyn tries
+	-- to use its own file watcher which consumes a million inotify
+	-- instances / file descriptors and blows up
+	--
+	-- is there a way to use neovim's file watching instead (something
+	-- to do with didChangeWatchedFiles?)
+	filewatching = false,
+
 });
