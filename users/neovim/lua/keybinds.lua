6286259ff123d17e25b612f7f4b13fcece4638e6
Nyctef <nyctef@nyctef.com>
2025-03-08 11:09:29 +0000

    disable custom go-to-definition keybind for now
    since C-] is is more standard and works in other contexts too

--- users/neovim/lua/keybinds.lua
+++ users/neovim/lua/keybinds.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = args.buf }
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts) -- try to use <C-]> instead
 
     vim.keymap.set({'n', 'x'}, 'gq', function() vim.lsp.buf.format({async = false, timeout_ms = 3000}) end, opts)
     vim.keymap.set({'n', 'x'}, 'ga', vim.lsp.buf.code_action, opts)
