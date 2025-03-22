0ada0828d24d05622cdd65a98c1da38f3f4555a7
Nyctef <nyctef@nyctef.com>
2025-03-22 09:36:52 +0000

    extract waitcat out into its own utils file

--- users/generic.nix
+++ users/generic.nix
@@ -10,23 +10,7 @@ with lib;
 let
   cfg = config.genHome;
 
-  waitcat = pkgs.writeShellScriptBin "waitcat" ''
-    #!/bin/sh
-
-    if [ -z "$1" ]; then
-      echo "Usage: $0 <filename> <...cat args>" >&2
-      exit 1
-    fi
-    filename="$1"
-    shift # strip off $1 so remaining args are left
-
-    until [ -f "$filename" ]
-    do
-      sleep 1
-    done
-
-    cat "$filename" "$@"
-    '';
+  waitcat = import ../utils/waitcat.nix { inherit pkgs; };
 in
 {
 
---
+++ utils/waitcat.nix
@@ -0,0 +1,19 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "waitcat" ''
+    #!/bin/sh
+
+    if [ -z "$1" ]; then
+      echo "Usage: $0 <filename> <...cat args>" >&2
+      exit 1
+    fi
+    filename="$1"
+    shift # strip off $1 so remaining args are left
+
+    until [ -f "$filename" ]
+    do
+      sleep 1
+    done
+
+    cat "$filename" "$@"
+    ''
