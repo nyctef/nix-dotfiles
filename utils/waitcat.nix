ed78aac02b5d3b236ab6e9f7a31fbb7afcd3d48c
Nyctef <nyctef@nyctef.com>
2025-03-22 09:59:15 +0000

    nix fmt

--- utils/waitcat.nix
+++ utils/waitcat.nix
@@ -1,19 +1,19 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "waitcat" ''
-    #!/bin/sh
+  #!/bin/sh
 
-    if [ -z "$1" ]; then
-      echo "Usage: $0 <filename> <...cat args>" >&2
-      exit 1
-    fi
-    filename="$1"
-    shift # strip off $1 so remaining args are left
+  if [ -z "$1" ]; then
+    echo "Usage: $0 <filename> <...cat args>" >&2
+    exit 1
+  fi
+  filename="$1"
+  shift # strip off $1 so remaining args are left
 
-    until [ -f "$filename" ]
-    do
-      sleep 1
-    done
+  until [ -f "$filename" ]
+  do
+    sleep 1
+  done
 
-    cat "$filename" "$@"
-    ''
+  cat "$filename" "$@"
+''
