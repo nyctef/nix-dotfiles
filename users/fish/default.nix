118a46c005bc378790d966c568cc9fcc4dfc7055
Nyctef <nyctef@nyctef.com>
2025-04-02 08:14:58 +0000

    add an abbreviation for a test postgres docker instance

--- users/fish/default.nix
+++ users/fish/default.nix
@@ -4,10 +4,15 @@
 {
   config = {
     # install fish, and tell HM to manage it (set session variables etc)
-    programs.fish.enable = true;
-    programs.fish.shellInit = "
-      set -U fish_features qmark-noglob
-    ";
+    programs.fish = { 
+      enable = true;
+      shellInit = "
+        set -U fish_features qmark-noglob
+        ";
+      shellAbbrs = {
+        pg = "docker run -d --name pg -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:latest";
+      };
+    };
     xdg.configFile."fish" = {
       source = ./.;
       recursive = true;
