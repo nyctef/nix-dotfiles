c5850addc794bbf80095d0fa78394c8102814862
Nyctef <nyctef@nyctef.com>
2026-01-29 10:59:08 +0000

    update treesitter-highlight config to handle breaking change

--- users/neovim/after/plugin/treesitter.lua
+++ users/neovim/after/plugin/treesitter.lua
@@ -1,8 +1,8 @@
--- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#modules
-require('nvim-treesitter.configs').setup({
-
-  highlight = {
-    enable = true,
-  },
-
-});
+-- https://github.com/nvim-treesitter/nvim-treesitter#highlighting
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('treesitter-highlight', { clear = true }),
+  pattern = '*',
+  callback = function(event)
+    pcall(vim.treesitter.start, event.buf, event.match)
+  end
+})
