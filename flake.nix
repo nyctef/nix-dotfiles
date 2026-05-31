b123d7e1fd0a5e0b9ca82e6b1be7fe627dbebc7a
Nyctef <nyctef@nyctef.com>
2026-05-31 14:20:24 +0000

    flake: pin home-manager to release-26.05 to match nixpkgs (nixos-unstable 26.05)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
