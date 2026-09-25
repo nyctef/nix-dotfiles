f7eaedf10afa8a25be610e50f0ecf316a79ca2b0
Nyctef <nyctef@nyctef.com>
2026-09-25 15:29:11 +0000

    set sweetiebot secrets (and remove pg db since it's external)

--- system/nyc-08/sweetiebot.nix
+++ system/nyc-08/sweetiebot.nix
@@ -6,33 +6,12 @@
     inputs.sweetiebot.nixosModules.default
   ];
 
-  # contains SB_JID, SB_PASSWORD and SB_CHATROOM
+  # contains: SB_JID, SB_CHATROOM, SB_PASSWORD, SB_NICKNAME, SB_PG_DB and SB_APPINSIGHTS_KEY
   age.secrets.sweetiebot-env.file = ../../secrets/sweetiebot-env.age;
 
   services.sweetiebot = {
     enable = true;
     environmentFile = config.age.secrets.sweetiebot-env.path;
-    settings = {
-      # peer auth over the unix socket: the service's DynamicUser is named
-      # after the unit, which matches the postgres role below
-      SB_PG_DB = "host=/run/postgresql dbname=sweetiebot user=sweetiebot";
-    };
   };
 
-  # the schema isn't created automatically: see bootstrap.md
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "sweetiebot" ];
-    ensureUsers = [
-      {
-        name = "sweetiebot";
-        ensureDBOwnership = true;
-      }
-    ];
-  };
-
-  systemd.services.sweetiebot = {
-    after = [ "postgresql.service" ];
-    requires = [ "postgresql.service" ];
-  };
 }
